15b7a144e3faa5a7bcc3b477ff3b459b7254fa00
Maria <marymarchenkova@gmail.com>
2018-11-27 22:04:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
+import './index.css';
+import data from './data/data';
+
+function Input(props) {
+  const [value, setValue] = useState("");
+  const [showSuggestion, setShow] = useState(false);
+
+  const inputHandler = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+    setShow(true);
+  };
+
+  const autoComplete = (sugg, input) => (sugg.filter(s => s.toLowerCase().includes(input.toLowerCase())));
+  const autoCompleteCheck = (sugg, input) => (sugg.filter(s => s.toLowerCase().includes(input.toLowerCase())) == input.toLowerCase());
+
+  const matchedSugg = autoComplete(props.suggestions, value);
+  const check = autoCompleteCheck(props.suggestions, value);
+
+  const song = matchedSugg.map((item) => <li>{item}</li>);
+
+  return (
+    <div className="Input">
+      <h1>Find your favorite Queen song!</h1>
+      <h1>🕺</h1>
+      <input value={value} onChange={inputHandler} className="search" />
+      <ul className="suggestionList">{showSuggestion && value!="" ? song : null}</ul>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Input suggestions={data} />
       </div>
     );
   }
---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+const data = ["We Are The Champions", "We Will Rock you", "Don't Stop Me Now", "Another One Bites the Dust",
+"Somebody To Love", "I Want To Break Free", "Killer Queen", "Crazy Little Thing Called Love", "Love of My Life", 
+"Who Wants to Live Forever", "Under Pressure", "Fat Bottomed Girls", "You're My Best Friend", "I Want It All", 
+"Bohemian Rhapsody", "Headlong", "Liar", "Let Me Live", "The Hitman", "White Queen", "You're My Best Friend", "Save Me", 
+"Spread Your Wings", "The Invisible Man", "The Miracle", "These Are The Days of Our Lives", "White Man", "'39", "A Kind of Magic", 
+"Bijou", "Bicycle Race", "Bosy Language", "Coming Soon", "Delilah", "Don't Try Suicide", "Flash", 
+"Hammer to Fall", "I Go Crazy", "Innuendo", "Is This The World We Created?", "Jesus", "Keep Yourself Alive", 
+"Lazing On A Sunday Afternoon", "Mother Love", "My Fairy King", "Ogre Battle"] 
+
+export default data;
+
